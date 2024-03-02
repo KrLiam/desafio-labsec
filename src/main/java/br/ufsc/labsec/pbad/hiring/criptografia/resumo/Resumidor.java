97eab2200eeefde4384d804e006eb66c47638d1d
KrLiam <88288206+KrLiam@users.noreply.github.com>
2024-03-02 14:43:04 -0300

    implementa Resumidor.resumir

--- src/main/java/br/ufsc/labsec/pbad/hiring/criptografia/resumo/Resumidor.java
+++ src/main/java/br/ufsc/labsec/pbad/hiring/criptografia/resumo/Resumidor.java
@@ -1,7 +1,11 @@
 package br.ufsc.labsec.pbad.hiring.criptografia.resumo;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Classe responsável por executar a função de resumo criptográfico.
@@ -16,8 +20,9 @@ public class Resumidor {
     /**
      * Construtor.
      */
-    public Resumidor() {
-        // TODO implementar
+    public Resumidor(String algoritmo) throws NoSuchAlgorithmException {
+        this.algoritmo = algoritmo;
+        md = MessageDigest.getInstance(this.algoritmo);
     }
 
     /**
@@ -26,9 +31,21 @@ public class Resumidor {
      * @param arquivoDeEntrada arquivo a ser processado.
      * @return Bytes do resumo.
      */
-    public byte[] resumir(File arquivoDeEntrada) {
-        // TODO implementar
-        return null;
+    public byte[] resumir(File arquivoDeEntrada) throws FileNotFoundException, IOException {
+        FileInputStream stream = new FileInputStream(arquivoDeEntrada);
+        
+        byte[] buffer = new byte[256];
+        md.reset();
+
+        while(true) {
+            int length = stream.read(buffer);
+            if (length == -1) break;
+
+            md.update(buffer, 0, length);
+        }
+
+        stream.close();
+        return md.digest();
     }
 
     /**
